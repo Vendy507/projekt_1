e72a424b4b3b026e874126fdfe142baece34ede2
Vendy507 <vendula.macikova@seznam.cz>
2024-12-10 20:51:20 +0100

    Řádek 14

---
+++ script1.py
@@ -0,0 +1,15 @@
+
+#slovník
+czech_republic = {
+    "population" : 10_000_000, 
+    "capital" : "Prague", 
+    "language" : "czech"
+}
+print(czech_republic["language"])
+
+#set
+zensky_rod = {"zena", "růže", "písen", "kost", "kost"}
+print(zensky_rod)
+
+
+
